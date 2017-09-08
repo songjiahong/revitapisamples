12ece15edb12b354c3b8b208321405160bf1acd8
Jiahong (Bruce) Song <song.jiahong@gmail.com>
2017-09-08 15:42:26 +1000

    Update FBXExporter assembly info

--- 1.6.FBXExporter/Properties/AssemblyInfo.cs
+++ 1.6.FBXExporter/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FBXExporter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Export all Revit files (rvt) in a given folder as fbx files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("revitapp.com")]
 [assembly: AssemblyProduct("FBXExporter")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © revitapp.com 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
