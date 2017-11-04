81e9377ee9559d1bbb10ba891b8dec5014e85576
Jiahong (Bruce) Song <song.jiahong@gmail.com>
2017-11-04 21:26:32 +1100

    Customized Ribbon Tab and Button

---
+++ 1.10.BatchUpgrader/Application.cs
@@ -0,0 +1,51 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BatchUpgrader
+{
+   [Transaction(TransactionMode.Manual)]
+   [Regeneration(RegenerationOption.Manual)]
+   public class Application : IExternalApplication
+   {
+      public Result OnStartup(UIControlledApplication application)
+      {
+         Assembly myAssembly = typeof(Application).Assembly;
+         string assemblyPath = myAssembly.Location;
+
+         // Create Ribbon Tab
+         application.CreateRibbonTab("MyAPP");
+
+         // Create Ribbon Panel to host the button
+         RibbonPanel panel = application.CreateRibbonPanel("MyAPP", "MyPanel");
+
+         // Create the push button
+         PushButton button = panel.AddItem(new PushButtonData("Upgrade", "BatchUpgrader", assemblyPath, "BatchUpgrader.Command")) as PushButton;
+         button.LargeImage = GetEmbeddedImage(myAssembly, "BatchUpgrader.Icons.Upgrade32.png");
+         button.Image = GetEmbeddedImage(myAssembly, "BatchUpgrader.Icons.Upgrade16.png");
+         button.ToolTip = "Batch upgrade all Revit files (rvt or rfa) in a given folder.";
+
+         return Result.Succeeded;
+      }
+
+      public Result OnShutdown(UIControlledApplication application)
+      {
+         return Result.Succeeded;
+      }
+
+      private ImageSource GetEmbeddedImage(System.Reflection.Assembly assemb, string imageName)
+      {
+         System.IO.Stream file = assemb.GetManifestResourceStream(imageName);
+         PngBitmapDecoder bd = new PngBitmapDecoder(file, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+
+         return bd.Frames[0];
+      }
+   }
+}
---
+++ 1.10.BatchUpgrader/README.md
@@ -0,0 +1,17 @@
+# BatchUpgrader
+Batch upgrade all Revit files (rvt or rfa) in a given folder.
+
+# Features
+* Upgrade all files in given folder
+* Search all sub directories
+* Specify target saving folder which is different from source folder and is not a parent or child folder of the source folder
+* Could choose to upgrade rvt file only or rfa file only or both
+* Show upgrade progress
+* Record exception log
+* Show summary information when finish
+* Use customized Ribbon Tab
+* Use customized Ribbon button with icon
+
+# Screenshot
+
+![batchupgrader](https://user-images.githubusercontent.com/7886579/32404598-99e4be6c-c1a6-11e7-8dfe-812f695b9cf1.png)
