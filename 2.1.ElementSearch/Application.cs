9faf86312a9e01078180408deae3e1eb794c7984
Jiahong (Bruce) Song <song.jiahong@gmail.com>
2018-03-01 21:09:50 +1100

    Remove unused namespaces

--- 2.1.ElementSearch/Application.cs
+++ 2.1.ElementSearch/Application.cs
@@ -2,11 +2,7 @@
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
