8c6f4c9ce74a35614aa09dc7376eacf8ef3bf26a
Jiahong (Bruce) Song <song.jiahong@gmail.com>
2019-01-28 00:04:36 +1100

    Load family

---
+++ 2.3.FamilyLoader/Commands.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace FamilyLoader
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class CommandLoader : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            var filePath = @"C:\TempFiles\";
+
+            try
+            {
+                var rfaPath = @"C:\TempFiles\Test_Fixed.rfa";
+                var symbolName = "0610 x 1220mm";
+                FamilySymbol symbol;
+
+                using (var t = new Transaction(doc, "Load symbol"))
+                {
+                    t.Start();
+                    var isSuccess = doc.LoadFamilySymbol(rfaPath, symbolName, new DefaultFamilyLoadOptions(), out symbol);
+
+                    if (isSuccess)
+                    {
+                        t.Commit();
+                        TaskDialog.Show("Info", "Load Symbol Success!" + symbol.Id.ToString());
+                    }
+                    else
+                    {
+                        t.RollBack();
+                        TaskDialog.Show("Info", "Load Symbol fail");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error", ex.Message);
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ 2.3.FamilyLoader/DefaultFamilyLoadOptions.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.DB;
+
+namespace FamilyLoader
+{
+    public class DefaultFamilyLoadOptions : IFamilyLoadOptions
+    {
+        public bool OnFamilyFound(bool familyInUse, out bool overwriteParameterValues)
+        {
+            overwriteParameterValues = !familyInUse;
+
+            return overwriteParameterValues;
+        }
+
+        public bool OnSharedFamilyFound(Family sharedFamily, bool familyInUse, out FamilySource source, out bool overwriteParameterValues)
+        {
+            if (familyInUse)
+            {
+                source = FamilySource.Project;
+                overwriteParameterValues = false;
+            }
+            else
+            {
+                source = FamilySource.Family;
+                overwriteParameterValues = true;
+            }
+
+            return overwriteParameterValues;
+        }
+    }
+}
